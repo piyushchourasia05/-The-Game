1584bd4656fc762216d2ecc8ce1e20f90eb888c8
piyushchourasia05 <78714786+piyushchourasia05@users.noreply.github.com>
2021-11-23 22:22:27 +0530

    all things added

---
+++ Game/Assets/thirdpersoncontroller.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using StarterAssets;
+
+public class thirdpersoncontroller : MonoBehaviour
+{
+   [SerializeField] private CinemachineVirtualCamera aimFreeLookCamera;
+    
+    private void update()
+    {
+     
+    }
+
+}
